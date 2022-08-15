3c3fb831331196bc8391684d4dc7227947f754d5
HeHH <1320901208@qq.com>
2022-08-15 21:27:40 +0800

    Add files via upload

---
+++ 24小时内访问日志.sh
@@ -0,0 +1,16 @@
+#set -x
+#使用前请确保Cookie和CSRFToken文件有效，可从浏览器copy过来，CSRFToken是Cookie中的csrftoken字段
+cookie=`cat Cookie`
+csrftoken=`cat CSRFToken`
+if [ ! -d 24小时内访问日志 ];then
+  mkdir 24小时内访问日志
+fi
+for d in $(cat domains)
+do
+d=`echo $d | sed 's/\r//'`
+	for i in $(seq 2 24)
+	do
+		t=$(date -d "$i hour ago"  +"%Y%m%d%H")
+		curl --no-progress-meter 'https://www.365cyd.com/log_download/make_download_link' -H "Cookie: $cookie" -H "X-CSRFToken: $csrftoken" -H "Referer: https://www.365cyd.com/log_download/list/" --data-raw "log_type=access&date_index=$t&domain=$d&action_type=hour"|jq -r '.data.download_link'|xargs -i wget {} -O "24小时内访问日志/$d-access-$t.log.gz"
+	done
+done
---
+++ 昨天访问日志.sh
@@ -0,0 +1,14 @@
+#set -x
+#使用前请确保Cookie和CSRFToken文件有效，可从浏览器copy过来，CSRFToken是Cookie中的csrftoken字段
+t=$(date -d "yesterday" +%Y%m%d)
+#t=$(date -d "7 days ago" +%Y%m%d) #7天前
+cookie=`cat Cookie`
+csrftoken=`cat CSRFToken`
+if [ ! -d $t-access  ];then
+  mkdir $t-access
+fi
+for d in $(cat domains)
+do
+	d=`echo $d | sed 's/\r//'`
+	curl --no-progress-meter 'https://www.365cyd.com/log_download/make_download_link' -H "Cookie: $cookie" -H "X-CSRFToken: $csrftoken" -H "Referer: https://www.365cyd.com/log_download/list/" --data-raw "log_type=access&date_index=$t&domain=$d&action_type=day"|jq -r '.data.download_link'|xargs -i wget {} -O "$t-access/$d-access-$t.log.gz"
+done
